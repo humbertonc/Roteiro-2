c15abb6192288e6e64130ba2763cc49b9f24cfed
Humberto Navarro <54050212+humbertonc@users.noreply.github.com>
2019-12-04 15:13:10 -0300

    Create MesadeRestaurante.h

---
+++ MesadeRestaurante.h
@@ -0,0 +1,23 @@
+#ifndef MESADERESTAURANTE_H
+#define MESADERESTAURANTE_H
+#include "Pedido.h"
+#include <string>
+
+using namespace std;
+class MesaDeRestaurante
+{
+    public:
+        Pedido pedidos[50];
+
+        MesaDeRestaurante();
+        virtual ~MesaDeRestaurante();
+        void adicionaAoPedido(Pedido p);
+        void zeraPedidos();
+        double calculaPedidos();
+
+    protected:
+
+    private:
+};
+
+#endif // MESADERESTAURANTE_H
