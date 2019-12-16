2ea411fe4613b2599a1b150b70b36834905c1d4c
Humberto Navarro <54050212+humbertonc@users.noreply.github.com>
2019-12-16 20:03:54 -0300

    Update 1main.cpp

--- 1main.cpp
+++ 1main.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
 #include "Pedido.h"
 #include "MesaDeRestaurante.h"
+#include "RestauranteCaseiro.h"
 #include <string>
 
 using namespace std;
 
 int main()
 {
-    double conta;
-    MesaDeRestaurante mesa = MesaDeRestaurante();
-    Pedido p = Pedido(54, 2, 19.99, "Almoco completo");
-    mesa.adicionaAoPedido(p);
-    conta = mesa.calculaPedidos();
-    cout << conta << endl;
-    mesa.zeraPedidos();
-    conta = mesa.calculaPedidos();
-    cout << conta << endl;
+    RestauranteCaseiro rest1 = RestauranteCaseiro();
+    Pedido p1 = Pedido(54, 2, 19.99, "Almoco completo");
+    Pedido p2 = Pedido(12, 2, 7.99, "Sobremesa");
+
+    rest1.adicionaAoPedido(1, p1);
+    rest1.adicionaAoPedido(1, p2);
+
+    Pedido p3 = Pedido(38, 1, 4.50, "Suco grande");
+    Pedido p4 = Pedido(54, 1, 19.99, "Almoco completo");
+
+    rest1.adicionaAoPedido(2, p3);
+    rest1.adicionaAoPedido(2, p4);
+
+    cout << "O total arrecado pelo restaurante foi " << rest1.calculaTotalRestaurante() << endl;
+
     return 0;
 }
