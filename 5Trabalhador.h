c1b087bef27cd86aa62eaf0b7977ed14fd02cdbf
Humberto Navarro <54050212+humbertonc@users.noreply.github.com>
2019-12-16 19:15:14 -0300

    Create 5Trabalhador.h

---
+++ 5Trabalhador.h
@@ -0,0 +1,18 @@
+#ifndef TRABALHADOR_H
+#define TRABALHADOR_H
+#include <string>
+
+using namespace std;
+class Trabalhador
+{
+    public:
+        Trabalhador();
+        string nome;
+        float salario;
+
+    protected:
+
+    private:
+};
+
+#endif // TRABALHADOR_H
