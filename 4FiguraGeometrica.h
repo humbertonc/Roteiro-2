a4c2ad3ea6ad6f7bcc9db856d30e2424fb7e084a
Humberto Navarro <54050212+humbertonc@users.noreply.github.com>
2019-12-16 18:24:20 -0300

    Create 4FiguraGeometrica.h

---
+++ 4FiguraGeometrica.h
@@ -0,0 +1,18 @@
+#ifndef FIGURAGEOMETRICA_H
+#define FIGURAGEOMETRICA_H
+#include <string>
+
+using namespace std;
+class FiguraGeometrica
+{
+    public:
+        FiguraGeometrica();
+        string nome;
+        double calculaArea();
+
+    protected:
+
+    private:
+};
+
+#endif // FIGURAGEOMETRICA_H
