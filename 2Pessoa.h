8f43ef63cf623c56a9da63f626b5866c9a955e7a
Humberto Navarro <54050212+humbertonc@users.noreply.github.com>
2019-12-16 16:07:35 -0300

    Create 2Pessoa.h

---
+++ 2Pessoa.h
@@ -0,0 +1,27 @@
+#ifndef PESSOA_H
+#define PESSOA_H
+#include <string>
+#include "Endereco.h"
+
+using namespace std;
+class Pessoa
+{
+    public:
+        Pessoa(string n);
+        Pessoa(string n, Endereco ende, string tel);
+
+        string getNome();
+        string getTel();
+        Endereco getEnd();
+        void setNome(string n);
+        void setTel(string tel);
+        void setEnd(Endereco ende);
+
+    protected:
+
+    private:
+        string nome, telefone;
+        Endereco endereco;
+};
+
+#endif // PESSOA_H
